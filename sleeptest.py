e9f876e42e970d97bcf22e75c5f35caafd17bcc8
fred <yelotag@gmail.com>
2018-04-30 06:53:24 -0700

    add ...

--- sleeptest.py
+++ sleeptest.py
@@ -21,7 +21,7 @@ class SleepTest(Test):
         sleep_length = self.params.get('sleep_length', default=1)
         self.log.debug("Sleeping for %.2f seconds", sleep_length)
         time.sleep(sleep_length)
-        self.log.debug("finito.....")
+        self.log.debug("finito...")
 
 
 if __name__ == "__main__":
